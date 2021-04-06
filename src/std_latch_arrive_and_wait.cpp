bfe0d546e06f3040c31ceec743d4011ae421117a
Toxe <toxe@toxe.net>
2021-04-06 14:40:37 +0200

    Add std_latch_arrive_and_wait.cpp

---
+++ src/std_latch_arrive_and_wait.cpp
@@ -0,0 +1,44 @@
+#include <chrono>
+#include <iostream>
+#include <latch>
+#include <mutex>
+#include <numeric>
+#include <thread>
+#include <vector>
+
+#include "common/prime.h"
+
+std::mutex mtx;
+
+void calc_nth_prime(int n, std::vector<int>& primes, std::latch& latch)
+{
+    latch.arrive_and_wait();
+
+    int prime = nth_prime(n);
+
+    {
+        std::lock_guard<std::mutex> lock(mtx);
+        primes.push_back(prime);
+    }
+}
+
+int main()
+{
+    const auto t1 = std::chrono::high_resolution_clock::now();
+
+    std::latch latch(11);
+    std::vector<std::thread> threads;
+    std::vector<int> primes;
+
+    for (int n = 30000; n <= 30010; ++n)
+        threads.emplace_back(calc_nth_prime, n, std::ref(primes), std::ref(latch));
+
+    for (auto& t : threads)
+        t.join();
+
+    const auto t2 = std::chrono::high_resolution_clock::now();
+    const auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1);
+
+    std::cout << "sum of primes = " << std::accumulate(primes.begin(), primes.end(), 0) << '\n';
+    std::cout << "duration: " << ms.count() << "ms\n";
+}
