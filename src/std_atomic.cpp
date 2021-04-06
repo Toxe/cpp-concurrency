667b8bd512387aef4ed825ed5b2e02627faaa170
Toxe <toxe@toxe.net>
2021-04-06 13:01:16 +0200

    Add std_atomic.cpp

---
+++ src/std_atomic.cpp
@@ -0,0 +1,32 @@
+#include <atomic>
+#include <chrono>
+#include <iostream>
+#include <thread>
+#include <vector>
+
+#include "common/prime.h"
+
+void calc_nth_prime(int n, std::atomic<int>& sum_of_primes)
+{
+    sum_of_primes += nth_prime(n);
+}
+
+int main()
+{
+    const auto t1 = std::chrono::high_resolution_clock::now();
+
+    std::vector<std::thread> threads;
+    std::atomic<int> sum_of_primes = 0;
+
+    for (int n = 30000; n <= 30010; ++n)
+        threads.emplace_back(calc_nth_prime, n, std::ref(sum_of_primes));
+
+    for (auto& t : threads)
+        t.join();
+
+    const auto t2 = std::chrono::high_resolution_clock::now();
+    const auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1);
+
+    std::cout << "sum of primes = " << sum_of_primes << '\n';
+    std::cout << "duration: " << ms.count() << "ms\n";
+}
