1607fbe46fa909277459966ee130f64c270fb25f
Toxe <toxe@toxe.net>
2021-04-25 12:47:31 +0200

    Add mutex_timer.cpp

---
+++ src/mutex_timer.cpp
@@ -0,0 +1,49 @@
+#include <chrono>
+#include <mutex>
+#include <numeric>
+#include <thread>
+#include <vector>
+
+#include <spdlog/spdlog.h>
+
+#include "common/mutex_timer.h"
+#include "common/prime.h"
+
+std::mutex mtx;
+MutexTimer mutex_timer("lock_guard", "calc_nth_prime");
+
+void calc_nth_prime(int n, std::vector<int>& primes)
+{
+    int prime = nth_prime(n);
+
+    {
+        const auto t0 = std::chrono::high_resolution_clock::now();
+        std::lock_guard<std::mutex> lock(mtx);
+        mutex_timer.update(t0);
+        primes.push_back(prime);
+    }
+}
+
+int main()
+{
+    spdlog::set_level(spdlog::level::debug);
+
+    const auto t1 = std::chrono::high_resolution_clock::now();
+
+    std::vector<std::thread> threads;
+    std::vector<int> primes;
+
+    for (int n = 30000; n <= 30010; ++n)
+        threads.emplace_back(calc_nth_prime, n, std::ref(primes));
+
+    for (auto& t : threads)
+        t.join();
+
+    const auto t2 = std::chrono::high_resolution_clock::now();
+    const auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1);
+
+    spdlog::info("sum of primes = {}", std::accumulate(primes.begin(), primes.end(), 0));
+    spdlog::info("duration: {}ms", ms.count());
+
+    mutex_timer.summary();
+}
