9e2ad07fec3e54ebfd08a80dea5b7728b5ce572c
Toxe <toxe@toxe.net>
2021-10-19 14:52:05 +0200

    Add jthread_with_stop_token

---
+++ src/jthread_with_stop_token/main.cpp
@@ -0,0 +1,34 @@
+#include <chrono>
+#include <stop_token>
+#include <thread>
+#include <vector>
+
+#include <spdlog/spdlog.h>
+
+#include "prime.h"
+
+void calc_primes(std::stop_token st)
+{
+    spdlog::info("calc_primes: started");
+
+    std::vector<int> primes;
+    int n = 30000;
+
+    while (!st.stop_requested())
+        primes.push_back(nth_prime(n++));
+
+    spdlog::info("calc_primes: stop requested, calculated {} primes", primes.size());
+}
+
+int main()
+{
+    std::jthread thread(calc_primes);
+
+    spdlog::info("started thread, sleeping for 3 seconds...");
+    std::this_thread::sleep_for(std::chrono::seconds(3));
+    spdlog::info("done, requesting thread to stop...");
+
+    thread.request_stop();
+
+    spdlog::info("quit");
+}
